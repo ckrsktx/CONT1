565d06a47359c3dc820c1418cd09f5f5c2865dbc
ckrsktx <marciorocha.s@protonmail.com>
2025-10-31 08:32:14 -0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1238,7 +1238,7 @@ function init() {
 // Inicializar quando o DOM estiver pronto
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', init);
-} else 
+} else;
 
 /* ---------- TESTE LIMPEZA MENSAL ---------- */
 const TEST_MANAGER = {
